79b5b0a8c12382ad406e6955037149f9ca84f695
shivam9400 <shivam.reads9400@gmail.com>
2026-01-15 15:20:15 +0530

    removed some files from serp_api

--- GenAI_LLM/fmcg_product_coding/langgraph_crawlai_based/schema.py
+++
@@ -1,8 +0,0 @@
-'''  Defines the "Target" of your coding here. '''
-
-from pydantic import BaseModel, Field
-
-class FMCGProduct(BaseModel):
-    brand: str = Field(description="The brand name, e.g., Coca-Cola")
-    category: str = Field(description="High-level category, e.g., Beverages")
-    confidence: float = Field(description="Score from 0 to 1 on accuracy")
